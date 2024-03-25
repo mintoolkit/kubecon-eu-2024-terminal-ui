5955a3355ca187b0d0eb258c7155a851dc0e9a00
Ladislav Prskavec <ladislav@prskavec.net>
2024-03-25 07:24:12 +0100

    Refactor to tui package

---
+++ tui/keymap.go
@@ -0,0 +1,18 @@
+package tui
+
+import "github.com/charmbracelet/bubbles/key"
+
+// keyMap defines the keybindings for the app.
+type keyMap struct {
+	reload key.Binding
+	search key.Binding
+}
+
+// defaultKeyMap returns a set of default keybindings.
+func defaultKeyMap() keyMap {
+	return keyMap{
+		search: key.NewBinding(
+			key.WithKeys("/"),
+		),
+	}
+}
